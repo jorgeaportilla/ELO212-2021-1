ea5b5c8d2ceea66e21628e9765fdf88df92f2a88
Patricio Henriquez <49575726+patriciohenriqueze@users.noreply.github.com>
2021-05-21 14:22:36 -0400

    Add files via upload

---
+++ Sesion4_Staff/Sesion4_params.tcl
@@ -0,0 +1,19 @@
+# Activity names
+set ACTIVITIES [list Act1 Act2 Act3]
+# Class Number
+set PARALELO "2"
+# Experience Name
+set SESION "Sesion4"
+# Groups to grade -- Set first and last group to grade
+set FIRST_GROUP 1
+set LAST_GROUP 9
+
+# Errors by its code
+# Act1
+set ACT_ERRORS(Act1,1) "El numero desplegado no corresponde"
+set ACT_ERRORS(Act1,2) "Mas de un display encendido"
+# Act2 
+set ACT_ERRORS(Act2,1) "El contador no esta bien definido"
+# Act3
+set ACT_ERRORS(Act3,1) "El resultado no es correcto" 
+set ACT_ERRORS(Act3,2) "Los flags no son correctos"
